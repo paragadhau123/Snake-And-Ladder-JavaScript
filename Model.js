bd09d199d42a9559b3386729990cbfca748d1357
Parag Adhau <adhauparag64@gmail.com>
2020-09-26 00:03:04 +0530

    Added :Code to roll a die and to get die number

--- Model.js
+++ Model.js
@@ -6,7 +6,10 @@ class Model
        var END_POSITION = 100;
         
         console.log("Starting Position : "+ START_POSITION);
-		    console.log("Ending Position : "+ END_POSITION);
+        console.log("Ending Position : "+ END_POSITION);
+        
+        var Random = Math.floor((Math.random()*6)+1);
+        console.log("Die Number is :"+Random);
        }
     }
     module.exports = new Model();
