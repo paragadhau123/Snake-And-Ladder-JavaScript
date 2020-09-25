b42ed3b7c84f466b3f1b14e696252f6430a7a540
Parag Adhau <adhauparag64@gmail.com>
2020-09-26 00:57:11 +0530

    Added :Code for checking option no-play,ladder,snake

--- Model.js
+++ Model.js
@@ -4,12 +4,48 @@ class Model
       {
        var START_POSITION = 0;
        var END_POSITION = 100;
-        
+       var option = 0;
+       var diceNumber = 0;
+       var newPosition = START_POSITION;
+
         console.log("Starting Position : "+ START_POSITION);
         console.log("Ending Position : "+ END_POSITION);
         
+        var diceNumber =this.throwDice();
+        var option = this.optionCheck();
+        console.log("Die Number is :"+diceNumber);
+
+        if (option == 2)
+        {
+          newPosition = newPosition+diceNumber;
+          console.log("Ladder");
+          console.log("NewPosition is :"+newPosition);
+        }
+        else if (option == 3)
+        {
+          newPosition = newPosition-diceNumber;
+          console.log("Snake");
+          console.log("NewPosition is :"+newPosition);
+        }
+        else if (option == 1)
+        {
+          newPosition=newPosition;
+          console.log("No-Play");
+          console.log("NewPosition is :"+newPosition);
+        }
+      }
+
+       optionCheck()
+        {
+        var Random = Math.floor((Math.random()*3)+1);
+        return Random;
+        }
+
+         throwDice()
+         {
         var Random = Math.floor((Math.random()*6)+1);
-        console.log("Die Number is :"+Random);
-       }
+        return Random;
+         }
+
     }
     module.exports = new Model();
