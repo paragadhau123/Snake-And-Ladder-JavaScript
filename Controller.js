4c2d96e3be539c26084a3e0a108e13b03ddc9ce2
Parag Adhau <adhauparag64@gmail.com>
2020-09-25 23:38:40 +0530

    Added :Code for intialising the starting and ending point

--- Controller.js
+++ Controller.js
@@ -1,1 +1,11 @@
-console.log("Welcome To Snake And Ladder Simulation");
+class Controller
+{
+  main()
+{
+  console.log("Welcome To Snake And Ladder Simulation");
+  const print = require('./Model');
+  print.Simulation();               
+   }
+ }
+  const object = new Controller();
+  object.main();
---
+++ Model.js
@@ -0,0 +1,12 @@
+class Model
+{
+    Simulation()    
+      {
+       var START_POSITION = 0;
+       var END_POSITION = 100;
+        
+        console.log("Starting Position : "+ START_POSITION);
+		    console.log("Ending Position : "+ END_POSITION);
+       }
+    }
+    module.exports = new Model();
